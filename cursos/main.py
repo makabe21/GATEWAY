cca1a0538e3a31c1896b105f5e283bd4c576b287
makabe21 <110963458+makabe21@users.noreply.github.com>
2026-05-27 06:14:58 +0000

    segundo

--- cursos/main.py
+++ cursos/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/usuarios")
+@app.get("/cursos")
 def get_users():
     return {"id": 1, "titulo": "Matematicas"}, {"id": 2, "titulo": "progra"}
